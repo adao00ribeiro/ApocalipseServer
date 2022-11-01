b0948f75e36c7c061106327a8ba616cdd9dff4de
Adão Ribeiro <adao-eduardo@hotmail.com>
2022-10-31 22:23:34 -0300

    att

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -21,26 +21,57 @@ export default function Home() {
           <img src="/Logo.svg" ></img>
         </div>
 
-        <section className={styles.sectionProjetos}>
+        <section >
 
-          <h1>Projetos</h1>
+          <h1 className={styles.H1center}>Projetos</h1>
+
           <div className={styles.grid}>
             <CardProject />
             <CardProject />
             <CardProject />
           </div>
         </section>
+
         <section >
-          <h1 className={styles.h1SobreNos}>Sobre Nos</h1>
+          <h1 className={styles.H1center}>Sobre Nos</h1>
           <div className={styles.divsobrenos}>
             <strong>Desenvolvimento de jogos Indie para pessoa fisica ou empresas</strong>
             <strong>Nosso Objectivo é entregar o projeto na melhor qualidade possivel no menor tempo possivel</strong>
           </div>
         </section>
+
+        <section >
+          <h1 className={styles.H1center}>Equipe</h1>
+          <div className={styles.grid}>
+            <CardProject />
+            <CardProject />
+            <CardProject />
+          </div>
+        </section>
+
+        <section >
+          <h1 className={styles.H1center}>Contato</h1>
+          <form className={styles.form}>
+            <div className={styles.divinput} >
+              <input className={styles.input} type="text" placeholder='Email'></input>
+              <input className={styles.input} type="text" placeholder='Nome'></input>
+            </div>
+            <div >
+              <textarea className={styles.input} placeholder='Mensagem'></textarea>
+            </div>
+            <button className={styles.buttonEnviar} type="submit">
+              Enviar
+            </button>
+          </form>
+        </section>
+
+        <footer>
+          <div className={styles.divfooter}>
+          <h1>2022</h1>
+          </div>
+          
+        </footer>
       </div>
-
-
-
     </>
   )
 }
