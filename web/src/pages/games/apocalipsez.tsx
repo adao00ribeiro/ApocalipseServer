ad9b00375ad791a8a5017e873f5cc90c3099bf11
Adão Ribeiro <adao-eduardo@hotmail.com>
2023-09-07 18:21:12 -0300

    Update apocalipsez.tsx

--- web/src/pages/games/apocalipsez.tsx
+++ web/src/pages/games/apocalipsez.tsx
@@ -29,7 +29,7 @@ export default function Apocalipsez() {
                     </div>
 
                     <div className={styles.divDownload}>
-                        <Link href={"/Game.7z"} target="_blank" download>  <span>Download</span></Link>
+                        <Link href={"https://drive.usercontent.google.com/download?id=1cBbM8OXhMwGnHdLYOk4OPPXKAag45ek_&export=download&authuser=0&confirm=t&uuid=e95d389d-fd24-4221-a9d4-0e39177a4d18&at=APZUnTWoKB76hidopReDDCz5AI02:1694121590301"} target="_blank" download>  <span>Download</span></Link>
                     </div>
                 </div>
             </div >
